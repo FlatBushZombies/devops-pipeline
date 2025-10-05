010425e8c9a36f837105e9afe7948152daf75f97
FlatBushZombies <brianmakanjira18@gmail.com>
2025-10-05 12:26:04 +0200

    feat: add CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("DevOps CI/CD Pipleine")
+console.log("Running YAML Scripts")
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting Test....")
+setTimeout(() => 
+    console.log("Waiting 3 Secons"), 3000
+)
+console.log("Test Complete")
